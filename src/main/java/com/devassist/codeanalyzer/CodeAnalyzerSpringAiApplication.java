0bc9e81207e92af37b7e06673c9707e5d14ef777
Razamwende Saturnin S <r.saint@hotmail.fr>
2026-04-07 15:24:04 +0200

    feat: initial Spring Boot project

---
+++ src/main/java/com/devassist/codeanalyzer/CodeAnalyzerSpringAiApplication.java
@@ -0,0 +1,13 @@
+package com.devassist.codeanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeAnalyzerSpringAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeAnalyzerSpringAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devassist/codeanalyzer/CodeAnalyzerSpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devassist.codeanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeAnalyzerSpringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
