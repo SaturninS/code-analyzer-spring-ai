7d01caf09ae7bdc407ef24ac0b8b2904349db4b6
Razamwende Saturnin S <r.saint@hotmail.fr>
2026-04-09 17:12:36 +0200

    feat: ajout TUs

---
+++ src/test/java/com/devassist/codeanalyzer/application/CodeAnalysisServiceTest.java
@@ -0,0 +1,56 @@
+package com.devassist.codeanalyzer.application;
+
+import com.devassist.codeanalyzer.domain.model.CodeAnalysis;
+import com.devassist.codeanalyzer.domain.model.CodeSnippet;
+import com.devassist.codeanalyzer.domain.port.CodeAnalysisPort;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class CodeAnalysisServiceTest {
+    @Mock
+    private CodeAnalysisPort codeAnalysisPort;
+
+    @InjectMocks
+    private CodeAnalysisService codeAnalysisService;
+
+    @Test
+    void analyseShouldDelegateToPort(){
+        //Given
+       var snippet = new CodeSnippet("def foo(): pass", "python");
+       var expectedAnalysis = new CodeAnalysis(
+               "python",
+               List.of(),
+               List.of(),
+               List.of("Ajouter une docstring"),
+               "low",
+               7,
+               "Fonction vide sans logique"
+       );
+       when(codeAnalysisPort.analyze(snippet)).thenReturn(expectedAnalysis);
+
+       //when
+        var result = codeAnalysisService.analyze(snippet);
+        //Then
+        assertThat(result).isEqualTo(expectedAnalysis);
+        assertThat(result.overallScore()).isEqualTo(7);
+        assertThat(result.language()).isEqualTo("python");
+    }
+
+    @Test
+    void summariz_shouldReturnSummaryFromPort(){
+        var snippet = new CodeSnippet("return a + b", "python");
+        when(codeAnalysisPort.summarize(snippet)).thenReturn("Additionne deux valeurs");
+        var result = codeAnalysisService.summarize(snippet);
+        assertThat(result).isEqualTo("Additionne deux valeurs");
+    }
+
+}
